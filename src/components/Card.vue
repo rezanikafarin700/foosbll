0abf8675d8dffb0b08ae4995e0a74260881ba7a4
rezanikafarin <rezanikafarin100@gmail.com>
2021-09-03 16:00:35 +0430

    create BEM structure in Card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,20 +1,20 @@
 <template>
 	<div>
 		<div class="card">
-			<div class="overlay"></div>
-			<div class="image" :style="{backgroundImage : `url(http://localhost/Asia/public/images/products/${product.id}/${product.image})`}">
-				<div class="ratio"></div>
+			<div class="card__overlay"></div>
+			<div class="card__image" :style="{backgroundImage : `url(http://localhost/Asia/public/images/products/${product.id}/${product.image})`}">
+				<div class="card__image--ratio"></div>
 			</div>
 
-			<div class="intro">
-				<div class="title">{{ product.name }}</div>
-				<div class="text">
+			<div class="card__intro">
+				<div class="card__title">{{ product.name }}</div>
+				<div class="card__text">
 					<div>قیمت : {{product.price.toLocaleString('fa')  }} تومان</div>
 					<div>اندازه : {{ product.size}}</div>
 					<div>مدل : {{ product.model }}</div>
 					<div>کد محصول : {{product.code}}</div>
 				</div>
-				<div class="btn-card">بیشتر </div>
+				<div class="card__btn-card">بیشتر </div>
 			</div>
 		</div>
 	</div>
@@ -44,7 +44,7 @@
 		box-shadow: 0 0 5px 0 #999999;
 		position: relative;
 
-		.overlay{
+		&__overlay{
 			width: 100%;
 			height: 100%;
 			background-color: rgba(0,0,0,.2);
@@ -58,7 +58,7 @@
 		&:hover{
 			z-index: -1;
 
-			.overlay{
+			&__overlay{
 				opacity: 1;
 				z-index: 1;
 			}
@@ -66,51 +66,51 @@
 
 
 
-		.image {
+		&__image {
 			background-position: center;
 			background-repeat: no-repeat;
 			background-size: cover;
 
-			.ratio {
+			&--ratio {
 				padding-top: 80%;
 				position: relative;
 			}
 		}
 
-		.intro {
+		&__intro {
 			padding: 1rem;
 			direction: rtl;
 			text-align: right;
 			transition: all .2s;
 			background-color: #fff;
-			/*z-index: 1;*/
 
-			.title {
-				font-size: 1.2rem;
-				font-weight: 500;
-				color: $color-title;
-				margin-bottom: 1.5rem;
-			}
-
-			.text {
-				font-size: 14px;
-				letter-spacing: .1rem;
-				line-height: 1.1;
-				color: darken($color-text, 1%);
-				margin-bottom: 2.5rem;
-			}
-
-			.btn-card {
-				width: 100%;
-				padding: .5rem 1.5rem;
-				background-color: $color-btn-background;
-				color: $color-btn-text;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-
-			}
 		}
+		&__title {
+			font-size: 1.2rem;
+			font-weight: 500;
+			color: $color-title;
+			margin-bottom: 1.5rem;
+		}
+
+		&__text {
+			font-size: 14px;
+			letter-spacing: .1rem;
+			line-height: 1.1;
+			color: darken($color-text, 1%);
+			margin-bottom: 2.5rem;
+		}
+
+		&__btn-card {
+			width: 100%;
+			padding: .5rem 1.5rem;
+			background-color: $color-btn-background;
+			color: $color-btn-text;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+		}
+
 	}
 
 </style>
